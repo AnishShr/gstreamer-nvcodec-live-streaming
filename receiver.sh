94f749430a593a2c38ffca891b58e2307c61f421
Tambet Matiisen <tambet.matiisen@ut.ee>
2026-06-22 17:49:17 +0200

    More updates.

--- receiver.sh
+++ receiver.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gst-launch-1.0 udpsrc port=5000 caps="application/x-rtp,media=video,encoding-name=H264,payload=96" ! rtpjitterbuffer latency=150 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! autovideosink sync=false
+gst-launch-1.0 udpsrc port=5000 \
+    caps="application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96" \
+    ! rtpjitterbuffer latency=400 do-lost=true \
+    ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! autovideosink sync=false
--- sender_vbr.sh
+++ sender_vbr.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 gst-launch-1.0 ximagesrc xid=$1 use-damage=false \
     ! videoconvert ! video/x-raw,format=NV12 \
-    ! nvh264enc preset=low-latency-hq rc-mode=cbr bitrate=3000 gop-size=30 \
+    ! nvh264enc preset=p4 tune=ultra-low-latency rc-mode=vbr bitrate=3000 max-bitrate=5000 gop-size=30 \
     ! h264parse config-interval=-1 \
     ! rtph264pay pt=96 config-interval=-1 mtu=1200 \
     ! udpsink host=10.0.1.2 port=5000 sync=false
-    
