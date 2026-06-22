626cffb4b39d0cb0ec6ce11d9f94d795256d5992
Tambet Matiisen <tambet.matiisen@ut.ee>
2026-06-22 16:58:42 +0200

    Fixed screen size.

--- sender_ffmpeg_vlc.sh
+++ sender_ffmpeg_vlc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -f x11grab -framerate 30 -i :0.0 -c:v h264_nvenc -preset llhq -rc cbr -zerolatency 1 -b:v 6000k -maxrate 6000k -bufsize 3000k -g 30 -bf 0 -pix_fmt yuv420p -f mpegts "udp://10.0.1.2:5000?pkt_size=1316"
+ffmpeg -f x11grab -framerate 30 -video_size "$(xdpyinfo | awk '/dimensions:/{print $2}')" -i :0.0 -c:v h264_nvenc -preset llhq -rc cbr -zerolatency 1 -b:v 6000k -maxrate 6000k -bufsize 3000k -g 30 -bf 0 -pix_fmt yuv420p -f mpegts "udp://10.0.1.2:5000?pkt_size=1316"
